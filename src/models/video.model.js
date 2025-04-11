51ff172fb15786d2e33b456e92bba7b8dde0f3d1
Ravi Varujana <raviprajapati45678@gmail.com>
2025-04-11 13:56:39 +0530

    created user model and video model, used hooks and added methods in user model, added keys in env variable

---
+++ src/models/video.model.js
@@ -0,0 +1,47 @@
+import mongoose, { Schema } from "mongoose";
+import aggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    duration: {
+      type: Number,
+      /* When claudinary(other cloud services may as well) stores some file, it automatically provides the data in case here the video duration */
+      required: true,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    isPublished: {
+      type: Boolean,
+      default: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    videoFile: {
+      type: String,
+      required: true,
+    },
+    thumbnail: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+videoSchema.plugin(aggregatePaginate);
+
+export const Video = mongoose.model("Video", videoSchema);
