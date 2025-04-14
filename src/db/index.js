e1575f3a4c62406b3c47391926252cb43e7a16a6
Ravi Varujana <raviprajapati45678@gmail.com>
2025-04-14 16:26:37 +0530

    added file upload code

--- src/db/index.js
+++ src/db/index.js
@@ -13,6 +13,10 @@ async function connectDB() {
     console.log(
       `Loggin mongoDB connection instance ${connectionInstance.connection.host}`
     );
+
+    /* This is not neccesary but we can utilize the mongoose.connection events to listen to events related to connection like loosing connectivity to mongodb server - this is basically used when an error occurs after the initial connection has been established - we can either use it here or in the index file on the top/globally*/
+
+    // mongoose.connection.on("error", () => console.error(err));
   } catch (err) {
     console.log(`MongoDB connection catch block ${err}`);
     // If the connection fails we can exit process immediately
---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,20 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    /*  */
+    cb(null, "./public/temp");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+
+    /* We can save the file in the temp storage on the server with any name we want */
+    // default code from multer
+    // cb(null, file.fieldname + "-" + uniqueSuffix);
+
+    /* We are keeping the original name of the file while saving it temporarily on the server - though having the same name would also mean that file will get override - but since we are performing cloud upload option instantly and unlinking the file on the successful upload, so it should not cause problems. */
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
