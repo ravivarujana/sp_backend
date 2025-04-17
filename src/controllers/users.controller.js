9a2388ca97f1e971694e4824e17bf2bb897046c5
Ravi Varujana <raviprajapati45678@gmail.com>
2025-04-17 22:56:54 +0530

    added register user controller

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,4 +1,8 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { User } from "../models/user.model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
 /* We need to pass a callback to our route handler, so the business login habdled for a specific route is written here */
 // const registerUser = (req, res) => {
@@ -8,8 +12,69 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 // };
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "Success",
+  /* Check Request Body */
+  if (!Object.keys(req.body).length) {
+    throw new ApiError(400, "Empty request body");
+  }
+
+  const { username, email, fullName, password } = req.body;
+
+  if (
+    [username, email, fullName, password].some((value) => value?.trim() === "")
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  /* Check the db methods - find if the user already exists*/
+  const isUserExist = await User.findOne({
+    $or: [{ username }, { email }],
   });
+
+  console.log("IS USER EXISTS ===== ", isUserExist);
+
+  if (isUserExist) {
+    throw new ApiError(409, "User already exists");
+  }
+
+  // const {avatar, coverImage} = req.files;
+  const avatarLocalPath = req.files?.avatar?.[0]?.path;
+  const coverImageLocalPath = req.files?.coverImage?.[0]?.path;
+
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar image is not present");
+  }
+
+  const uploadAvatar = await uploadOnCloudinary(avatarLocalPath);
+  const uploadCoverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+  console.log("UPLOAD AVATAR ON CLAUDINARY  +++++++++++ ", uploadAvatar);
+
+  /* What error code should be there as this is third party service */
+  if (!uploadAvatar) {
+    throw new ApiError(400, "Avatar image is not present");
+  }
+
+  const createUser = await User.create({
+    fullName,
+    avatar: uploadAvatar?.url,
+    email,
+    password,
+    username: username.trim().toLowerCase(),
+    coverImage: uploadCoverImage?.url || "",
+  });
+
+  /* Making extra DB call to check if user has been successfully added to DB */
+  /* Select query here bydefault have all the fields selected, to not consider few fields user - sign as prefix inside string */
+  const userCreated = await User.findById(createUser._id).select(
+    "-password -refreshToken"
+  );
+
+  if (!userCreated) {
+    throw new ApiError(500, "Something went wrong while registering user");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, userCreated, "User successfully registered"));
 });
 export { registerUser };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,22 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/users.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
 
 console.log("inside the router file");
 
-router.route("/register").post(registerUser);
+/* Using multer middleware for handling the files - here image for avatar and coverImage  */
+
+/* This middeware will allow us to access the files on the request */
+
+/* Allowing user to only enter max 1 image for each field */
+router.route("/register").post(
+  upload.fields([
+    { name: "avatar", maxCount: 1 },
+    { name: "coverImage", maxCount: 1 },
+  ]),
+  registerUser
+);
 
 export default router;
